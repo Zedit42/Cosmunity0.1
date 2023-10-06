953b9c44e7d612fc2f9c2580c69ba42d67a624ca
Zedit42 <ygtglbgl@gmail.com>
2023-10-06 23:20:32 +0300

    First pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { VT323 } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const vt323 = VT323({ subsets: ['latin'], weight:'400' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="synthwave">
+      <body className={vt323.className}>{children}</body>
     </html>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,101 @@
+"use client"
+import React, { useState, useEffect, forwardRef, useImperativeHandle } from "react";
+import { StargateClient } from "@cosmjs/stargate";
+import ChatBox from '../components/ChatBox'
+
+export const ConnectWallet = forwardRef((_, ref) => {
+
+    // Usetstate for storing wallets details state.
+    const [cosmosAddress, setCosmosAddress] = useState("");
+    const [cosmosBalance, setCosmosBalance] = useState(null);
+    const [cosmosToken, setCosmosToken] = useState(null);
+  
+
+    const buttonHandlerKeplrConnect = async() => {
+
+        if (window.keplr) {
+
+            // Unlock the wallet.
+            await window.keplr.enable('akashnet-2'); 
+
+            // Use offlineSigner to get first wallet and public key.
+            // Currently only first address is supported.
+            const offlineSigner = await window.getOfflineSigner('akashnet-2');
+            const keplrAccounts = await offlineSigner.getAccounts();
+
+            // Set state value as first address.
+            setCosmosAddress(keplrAccounts[0].address);
+        } else {
+            alert("Keplr extension is not installed.");
+        }
+    };
+
+    // Button handler button for handling a balance request for Cosmos SDK wallet as RPC.
+    const buttonHandlerKeplrBalance = async() => {
+
+        const client = await StargateClient.connect('https://rpc.akashnet.net:443');
+
+        // Get balance as Coin.
+        // Amount is the number of coins, while denom is the identifier of the coins.
+        const balanceAsCoin = await client.getBalance(cosmosAddress, 'uakt');
+        const balance = parseInt(balanceAsCoin.amount) * 1/1e6;
+
+        // Set state values.
+        setCosmosBalance(balance);
+        setCosmosToken('AKT');
+    };
+    useEffect(() => {
+        console.log(cosmosAddress);
+      if (cosmosAddress!= undefined) {
+        buttonHandlerKeplrBalance();
+      }
+    
+    }, [cosmosAddress])
+    
+
+    // Clear cosmos balance data.
+    useImperativeHandle(ref, () => ({
+
+        handleClearUtils() {
+            setCosmosAddress("");
+            setCosmosBalance(null);
+            setCosmosToken("");
+        }
+    }));
+
+    return (
+        <>
+            <img
+                src="/bg.png"
+                className=" fixed w-screen min-w-[1024px] h-screen object-cover z-0"
+            />
+
+
+            {cosmosAddress ? (
+            <div className=" z-10 pixelbg max-w-[90%]">
+                {/* <label>
+                    Available balance of {cosmosAddress}: {cosmosBalance} {cosmosToken}
+                </label>
+                <button onClick={buttonHandlerKeplrBalance} variant="primary" disabled={!cosmosAddress}>
+                    Get balance
+                </button> */}
+                <ChatBox walletAddress={cosmosAddress} tokenAmount={cosmosBalance}/>
+            </div>
+            ) : (
+                <div className=" z-10 m-auto scale-150">
+                <div className=" text-white">
+                    <div className=" text-5xl text-center">
+                        Cosmunity
+                    </div>
+                    <div className=" text-center text-lg italic">
+                        Connect With Cosmonauts
+                    </div>
+                </div>
+                <button onClick={buttonHandlerKeplrConnect} data-testid="keplr-button" className=" pixelbg items-center text-xl">
+                    CONNECT WALLET
+                </button>
+                </div>
+            )}
+        </>
+    )
+})
